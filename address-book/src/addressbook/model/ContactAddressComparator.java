794a50cc6894235626f555792123c516b0e9fe27
iproduct <office@iproduct.org>
2016-09-14 18:11:59 +0300

    ContactAddressComparator added

---
+++ address-book/src/addressbook/model/ContactAddressComparator.java
@@ -0,0 +1,12 @@
+package addressbook.model;
+
+import java.util.Comparator;
+
+public class ContactAddressComparator implements Comparator<Contact> {
+
+	@Override
+	public int compare(Contact c1, Contact c2) {
+		return 0;
+	}
+
+}
