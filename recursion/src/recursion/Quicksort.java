db76218d91e57edb6bd1c6523a70d8e4a1831209
iproduct <office@iproduct.org>
2016-09-23 12:22:09 +0300

    quicksort aadded

---
+++ recursion/src/recursion/Quicksort.java
@@ -0,0 +1,45 @@
+package recursion;
+
+import java.util.Arrays;
+
+public class Quicksort {
+	
+	private static int[] a = { 7, 3, 1, 19, 0, 3, 5, 2, 11, 19, 6, 3, 1, 11, 7, 
+			16, 5, 13, 2, 1, 1, 5, 18, 56, 18, 22, 97, 3, 19, 4, 0,	7, 12 };
+
+	
+	public static void quickSort(int[] a) {
+		quickSort(a, 0, a.length - 1);
+	}
+
+	public static void quickSort(int[] a, int start, int end) {
+		if(start >= end) return; //recursion bottom
+		
+		//splitting array in two
+		int pivotValue = a[start];
+		int left = start;
+		int right = end + 1;
+		while(true){
+			do left++; while(a[left] < pivotValue);
+			do right--; while(a[right] > pivotValue);
+			if(right <= left) break;
+			swap(a, left, right);
+		}
+		swap(a, start, right);
+		
+		//recursion step
+		quickSort(a,start, right - 1);
+		quickSort(a,right + 1, end);
+	}
+	
+	public static void swap(int[] a, int i, int j) {
+		int temp = a[i];
+		a[i] = a[j];
+		a[j] = temp;
+	}
+	
+	public static void main(String[] args){
+		quickSort(a);
+		System.out.println(Arrays.toString(a));
+	}
+}
