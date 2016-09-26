77173f8a9d78ae24d8a9b83c8de42780abdb3abd
iproduct <office@iproduct.org>
2016-09-26 10:13:59 +0300

    calculations implemented

--- lists/src/stack/ReversePolishNotationCalculator.java
+++ lists/src/stack/ReversePolishNotationCalculator.java
@@ -1,6 +1,7 @@
 package stack;
 
 import java.util.Arrays;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class ReversePolishNotationCalculator {
@@ -13,8 +14,41 @@ public class ReversePolishNotationCalculator {
 		System.out.println(Arrays.toString(parts));
 		
 		//calculation
+		double a, b;
 		for(int i = 0; i < parts.length; i++){
-			
+			Matcher numberMatcher = numberPattern.matcher(parts[i]);
+			Matcher operationMatcher = operationPattern.matcher(parts[i]);
+			if( numberMatcher.matches()) {
+				double number = Double.parseDouble(parts[i]);
+				stack.push(number);
+			} else if (operationMatcher.matches() && parts[i].length() == 1) {
+				switch (parts[i]) {
+				case "+" :
+					a = stack.pop();
+					b = stack.pop();
+					stack.push(b + a);
+					break;
+				case "-" :
+					a = stack.pop();
+					b = stack.pop();
+					stack.push(b - a);
+					break;
+				case "*" :
+					a = stack.pop();
+					b = stack.pop();
+					stack.push(b * a);
+					break;
+				case "/" :
+					a = stack.pop();
+					b = stack.pop();
+					stack.push(b / a);
+					break;
+				case "~" :
+					a = stack.pop();
+					stack.push(-a);
+					break;
+				}
+			}
 		}
 		
 		
