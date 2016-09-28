791062afade7a12b14f24c5d7f41417c2f57ed7c
iproduct <office@iproduct.org>
2016-09-28 09:42:29 +0300

    Recursive search in BST

--- trees/src/binarytree/BSTree.java
+++ trees/src/binarytree/BSTree.java
@@ -23,6 +23,19 @@ public class BSTree<K extends Comparable<K>, V>  {
 		return root;
 	}
 	
+	public Entry<K,V> search(K key) {
+		return searchR(root, key);
+	}
+	
+	public Entry<K,V> searchR(BTNode<Entry<K,V>> root, K key) {
+		if(root == nil) return null;
+		if(root.entry.getKey().equals(key)) return root.entry;
+		if(key.compareTo(root.entry.getKey()) < 0) 
+			return searchR(root.left, key);
+		else if (key.compareTo(root.entry.getKey()) > 0)
+			return searchR(root.right, key);
+		return null;
+	}
 	
 	public void print() {
 		root.show();
--- trees/src/binarytree/TestBSTree.java
+++ trees/src/binarytree/TestBSTree.java
@@ -3,15 +3,20 @@ package binarytree;
 public class TestBSTree {
 
 	public static void main(String[] args) {
-		BSTree<Integer,Integer> bst = new BSTree<>();
+		BSTree<Integer,String> bst = new BSTree<>();
 //		bst.print();
-		bst.insert(new Entry<>(12,12));
-		bst.insert(new Entry<>(3,3));
-		bst.insert(new Entry<>(18,18));
-		bst.insert(new Entry<>(7,7));
-		bst.insert(new Entry<>(4,4));
+		bst.insert(new Entry<>(12,"twelve"));
+		bst.insert(new Entry<>(3,"three"));
+		bst.insert(new Entry<>(18,"eighteen"));
+		bst.insert(new Entry<>(7,"seven"));
+		bst.insert(new Entry<>(4,"four"));
+		
 		bst.print();
 		
+		Entry<Integer, String> found = bst.search(7);
+		
+		System.out.println((found == null) ? "not found"
+				: found.getKey() + ": " + found.getValue());
 
 	}
 
