baa460fdb1a7357bc45d25db52fd2b05d24f9d84
Trayan Iliev <office@iproduct.org>
2018-09-24 10:15:50 +0300

    options map added

--- address-book-lab1/src/addressbook/view/AddressBookPresentation.java
+++ address-book-lab1/src/addressbook/view/AddressBookPresentation.java
@@ -1,15 +1,32 @@
 package addressbook.view;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import addressbook.control.AddressBook;
 
 public class AddressBookPresentation {
+	private static final Object[][] options = {
+		{1, "List all contacts"},
+		{2, "Add contact"},
+		{3, "Find by first, last name"},
+		{4, "Find by part of first, last name or organization"},
+		{5, "Update contact"},
+		{6, "Delete contact"},
+		{9, "Exit"}
+	};
+	
 	private AddressBook controller;
-	
-	
+	private Map<Integer, String> optionsMap = new HashMap<>();
 
 	public AddressBookPresentation(AddressBook controller) {
 		this.controller = controller;
+		
 	}
+	
+//	public showMainMenu() {
+//	
+//	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
