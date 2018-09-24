365e7b8b02ae30218d8239ea6c4a912291fdd526
Trayan Iliev <office@iproduct.org>
2018-09-24 09:47:49 +0300

    presentation class added

---
+++ address-book-lab1/src/addressbook/view/AddressBookPresentation.java
@@ -0,0 +1,14 @@
+package addressbook.view;
+
+public class AddressBookPresentation {
+
+	public AddressBookPresentation() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
