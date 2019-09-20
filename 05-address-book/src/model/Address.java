6b772242fb8277cdd1de8c4447be27bb3ef58f1e
Trayan Iliev <office@iproduct.org>
2019-09-20 12:10:29 +0300

    05-address-book lab initial cpommit

---
+++ 05-address-book/src/model/Address.java
@@ -0,0 +1,85 @@
+package model;
+
+public class Address {
+	private String country;
+	private String city;
+	private String address;
+
+	public Address() {
+	}
+
+	public Address(String country, String city, String address) {
+		this.country = country;
+		this.city = city;
+		this.address = address;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((address == null) ? 0 : address.hashCode());
+		result = prime * result + ((city == null) ? 0 : city.hashCode());
+		result = prime * result + ((country == null) ? 0 : country.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Address other = (Address) obj;
+		if (address == null) {
+			if (other.address != null)
+				return false;
+		} else if (!address.equals(other.address))
+			return false;
+		if (city == null) {
+			if (other.city != null)
+				return false;
+		} else if (!city.equals(other.city))
+			return false;
+		if (country == null) {
+			if (other.country != null)
+				return false;
+		} else if (!country.equals(other.country))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append(country).append(", ").append(city).append(", ").append(address);
+		return builder.toString();
+	}
+
+}
---
+++ 05-address-book/src/model/Contact.java
@@ -0,0 +1,170 @@
+package model;
+
+import java.util.Date;
+
+public class Contact {
+	private String firstName;
+	private String lastName;
+	private String email;
+	private String mobilePhone;
+	private String businessPhone;
+	private String homePhone;
+	private String organization;
+	private Address address;
+	private String description;
+	private Date created;
+	private Date modified;
+	private String photoUrl;
+	public Contact() {
+	}
+	public Contact(String firstName, String lastName, String email, String mobilePhone) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.mobilePhone = mobilePhone;
+	}
+	public Contact(String firstName, String lastName, String email, String mobilePhone, String businessPhone,
+			String homePhone, String organization, Address address, String description, Date created, Date modified,
+			String photoUrl) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.mobilePhone = mobilePhone;
+		this.businessPhone = businessPhone;
+		this.homePhone = homePhone;
+		this.organization = organization;
+		this.address = address;
+		this.description = description;
+		this.created = created;
+		this.modified = modified;
+		this.photoUrl = photoUrl;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getMobilePhone() {
+		return mobilePhone;
+	}
+	public void setMobilePhone(String mobilePhone) {
+		this.mobilePhone = mobilePhone;
+	}
+	public String getBusinessPhone() {
+		return businessPhone;
+	}
+	public void setBusinessPhone(String businessPhone) {
+		this.businessPhone = businessPhone;
+	}
+	public String getHomePhone() {
+		return homePhone;
+	}
+	public void setHomePhone(String homePhone) {
+		this.homePhone = homePhone;
+	}
+	public String getOrganization() {
+		return organization;
+	}
+	public void setOrganization(String organization) {
+		this.organization = organization;
+	}
+	public Address getAddress() {
+		return address;
+	}
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public Date getCreated() {
+		return created;
+	}
+	public void setCreated(Date created) {
+		this.created = created;
+	}
+	public Date getModified() {
+		return modified;
+	}
+	public void setModified(Date modified) {
+		this.modified = modified;
+	}
+	public String getPhotoUrl() {
+		return photoUrl;
+	}
+	public void setPhotoUrl(String photoUrl) {
+		this.photoUrl = photoUrl;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
+		result = prime * result + ((lastName == null) ? 0 : lastName.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Contact other = (Contact) obj;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (firstName == null) {
+			if (other.firstName != null)
+				return false;
+		} else if (!firstName.equals(other.firstName))
+			return false;
+		if (lastName == null) {
+			if (other.lastName != null)
+				return false;
+		} else if (!lastName.equals(other.lastName))
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Name: ").append(firstName).append(" ").append(lastName)
+				.append("\nEmail: ").append(email).append("\nMobile: ").append(mobilePhone);
+		if(businessPhone != null)
+			builder.append(", Business: ").append(businessPhone);
+		if(homePhone != null)
+			builder.append(", Home: ").append(homePhone);
+		if(organization != null)
+			builder.append("\nOrganization: ").append(organization);
+		if(address != null)
+			builder.append("\nAddress=").append(address);
+		if(description != null)
+			builder.append("\nDescription=").append(description);
+		if(photoUrl != null)
+			builder.append("\nPhoto: ").append(photoUrl);
+		return builder.toString();
+	}
+	
+	
+}
