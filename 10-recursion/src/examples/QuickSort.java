fb1271d19ebaa1c44bfeb1de2ff3663c1f466065
Trayan Iliev <office@iproduct.org>
2019-09-26 10:00:03 +0300

    quicksort completed

--- 10-recursion/src/examples/QuickSort.java
+++ 10-recursion/src/examples/QuickSort.java
@@ -1,18 +1,25 @@
 package examples;
 
 import java.util.Arrays;
+import java.util.Random;
 
 public class QuickSort {
 	
-	public static void quickSort(int[] a, int start, int end) {
-		if(end - start <= 1) return; // recursion bottom
+	public static void quickSort(int[] a, int start, int end, int level) {
+		// trace the recursive execution
+		StringBuilder sb = new StringBuilder();
+		for(int k = 0; k < level; k++)
+			sb.append("  ");
+		System.out.printf("%s[%d, %d]: %s\n", sb.toString(), start, end,
+				Arrays.toString(Arrays.copyOfRange(a, start, end)));
 		
+		if(end - start <= 1) return; // recursion bottom
 		
 		int pivot = a[start];
 		int left = start + 1;
 		int right = end - 1;
 		
-		// move smaller to left and bigger to the right of pivot
+		// 1. Partition: move smaller to left and bigger to the right of pivot
 		while(true) {
 			while(left < right && a[left] <= pivot) left++;
 			while(left < right && a[right] >= pivot) right--;
@@ -23,9 +30,10 @@ public class QuickSort {
 		if(a[start] > a[right])
 			swap(a, start, right);
 		
-		// recursion step
-		quickSort(a, start, right);
-		quickSort(a, right, end);
+		// 2. Recursion step
+		level++;
+		quickSort(a, start, right, level);
+		quickSort(a, right, end, level);
 
 	}
 	
@@ -36,8 +44,13 @@ public class QuickSort {
 	}
 
 	public static void main(String[] args) {
-		int[] a = {23, 15, 17, 92, 110, 4, 12, 67, 54, 45};
-		quickSort(a, 0, a.length);
+//		int[] a = {23, 15, 17, 92, 110, 4, 12, 67, 54, 45};
+		Random rand = new Random();
+		int[] a = new int[100];
+		for(int i = 0; i < a.length; i++) {
+			a[i] = rand.nextInt(1000);
+		}
+		quickSort(a, 0, a.length, 0);
 		System.out.println(Arrays.toString(a));
 	}
 
